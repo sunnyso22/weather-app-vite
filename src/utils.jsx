3ca0f1511fd7cc1f9d277c6927badddf9e310608
Sunny So <sohiutung123456@yahoo.com.hk>
2024-10-17 16:14:21 +0800

    Added handleChange, handleSave and utils.jsx

---
+++ src/utils.jsx
@@ -0,0 +1,82 @@
+export const availableLocations= [
+    {
+        countyName: '宜蘭縣',
+        stationName: '宜蘭',
+    },
+    {
+        countyName: '嘉義市',
+        stationName: '嘉義',
+    },
+    {
+        countyName: '屏東縣',
+        stationName: '恆春',
+    },
+    {
+        countyName: '雲林縣',
+        stationName: '雲林',
+    },
+    {
+        countyName: '臺東縣',
+        stationName: '臺東',
+    },
+    {
+        countyName: '臺北市',
+        stationName: '臺北',
+    },
+    {
+        countyName: '金門縣',
+        stationName: '金門',
+    },
+    {
+        countyName: '桃園市',
+        stationName: '新屋',
+    },
+    {
+        countyName: '彰化縣',
+        stationName: '彰師大',
+    },
+    {
+        countyName: '嘉義縣',
+        stationName: '阿里山',
+    },
+    {
+        countyName: '高雄市',
+        stationName: '高雄',
+    },
+    {
+        countyName: '基隆市',
+        stationName: '基隆',
+    },
+    {
+        countyName: '臺南市',
+        stationName: '南區中心',
+    },
+    {
+        countyName: '南投縣',
+        stationName: '日月潭',
+    },
+    {
+        countyName: '臺中市',
+        stationName: '臺中',
+    },
+    {
+        countyName: '新竹縣',
+        stationName: '新竹',
+    },
+    {
+        countyName: '花蓮縣',
+        stationName: '花蓮',
+    },
+    {
+        countyName: '連江縣',
+        stationName: '馬祖',
+    },
+    {
+        countyName: '澎湖縣',
+        stationName: '澎湖',
+    },
+    {
+        countyName: '新北市',
+        stationName: '板橋',
+    },
+];
